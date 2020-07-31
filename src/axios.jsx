a9295d12b14349d6a8b66f20729fe1d99351bcc4
Bülent Güven <bulentguven97@hotmail.com>
2020-07-31 19:48:30 +0300

    fix on http

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export default Axios.create({
-    baseURL: "http://ddragon.leagueoflegends.com/cdn/10.15.1/data"
+    baseURL: "https://ddragon.leagueoflegends.com/cdn/10.15.1/data"
 
 });
